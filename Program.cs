ab9c4f4a12210515f19e3c371cfd2d54e5f86417
Markonezi <dusanmarkovic1021@gmail.com>
2025-07-21 23:11:43 +0200

    Added basic layout for project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,14 @@ namespace multithreaded_web_server
 {
     internal class Program
     {
+        private static readonly String root = "/Files";
+        private static readonly int PORT = 5050;
+        // <string,string> - GET method, response
+        private static ConcurrentDictionary<string, string> responseCache = new ConcurrentDictionary<string, string>();
+
         static void Main(string[] args)
         {
+            
         }
     }
 }
---
+++ Utils/FileHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multithreaded_web_server.Utils
+{
+    internal class FileHandler
+    {
+        /*
+            Checks files existance and creates them if it doesnts
+        */
+    }
+}
